c6dc8679c92b6b52e41bc5c1ae951be8b349a910
Victor Sabry <victorsabry@hotmail.com>
2023-02-25 17:11:20 +0100

    Renamed project

--- Car Unit TestTests/CarTests.cs
+++ Car Unit TestTests/CarTests.cs
@@ -9,12 +9,32 @@ using System.Threading.Tasks;
 namespace Car_Unit_Test.Tests
 {
     [TestClass()]
+
     public class CarTests
     {
+        Car myBadCar = new() { Id = 1, Model = "BMW", Price = -11, LicensePlate = "AAX98292" };
+        Car myPerfectCar = new() { Id = 2, Model = "B66jj", Price = 11, LicensePlate = "AAX9829" };
         [TestMethod()]
-        public void CarTest()
+        public void ValidateModelTest()
+        {            
+            myPerfectCar.ValidateModel();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { myBadCar.ValidateModel(); });
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { new Car { Model = null }.ValidateModel(); });
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
         {
-            Assert.Fail();
+            myPerfectCar.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { myBadCar.ValidatePrice(); });
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            myPerfectCar.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { myBadCar.ValidateLicensePlate(); });
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { new Car { LicensePlate = "X"}.ValidateLicensePlate(); });
         }
     }
 }
