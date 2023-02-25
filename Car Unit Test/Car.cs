149c85d0c67bbad8270f1d6560b0474333dbb23a
Victor Sabry <victorsabry@hotmail.com>
2023-02-25 16:50:02 +0100

    Add project files.

---
+++ Car Unit Test/Car.cs
@@ -0,0 +1,38 @@
+﻿namespace Car_Unit_Test
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public int Price { get; set; }
+        public string? LicensePlate { get; set; }
+
+
+        public void ValidateModel()
+        {
+            if (Model == null || Model.Length < 4)
+            {
+                throw new ArgumentOutOfRangeException($"Model name needs to be at least 4 characters long");
+            }
+        }
+
+        public void ValidatePrice()
+        {
+            if (Price <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"Price can't be less than zero, your price was {Price}");
+            }
+        }
+
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate.Length < 2 || LicensePlate.Length > 7)
+            {
+                throw new ArgumentOutOfRangeException($"Invalid plate length, needs to be between 2 and 7, your plate length was {LicensePlate.Length} characters long");
+            }
+        }
+
+        
+
+    }
+}
---
+++ Car Unit TestTests/CarTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Car_Unit_Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Unit_Test.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void CarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
